d1312ba9f13ac6af91df8ed9d3253c4c14c89675
Jeris Manning <the.goose.moose@gmail.com>
2019-08-18 21:48:34 -0600

    Answers short answer questions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
 // import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
-import AppRouter from "./components/AppRouter.js";
+import Header from './components/Header.js';
+import AppRouter from './components/AppRouter.js';
+// import SearchForm from "./components/SearchForm.js";
 
 export default function App() {
   return (
